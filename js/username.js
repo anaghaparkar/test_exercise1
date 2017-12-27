10159a1ba91bea0a4b097f24e35d797c40039fdc
anagha.p <anagha.p>
2017-12-27 23:15:57 +0530

    added test files

---
+++ js/username.js
@@ -0,0 +1,11 @@
+function alertUsername(){
+	var username = document.getElementById('txt_username').value;	
+	alert('Hi ' + username);
+}
+
+window.onload = function(){
+	//document.getElementById("demo").innerHTML = Date();
+	console.log("Today's Date: " + Date());
+}
+
+
